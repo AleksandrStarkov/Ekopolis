4a771d113070717532381228ca709e0a72d73110
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2023-04-07 13:57:21 +0300

    fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, styled, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { Container } from '@mui/system';
 
 import BackImg from '../../assets/khersoncleanup.jpg';
