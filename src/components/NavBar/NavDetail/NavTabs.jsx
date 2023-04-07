fdd9010d618002f1989a962903ddcac715fbd28e
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2023-04-07 17:09:12 +0300

    fix

--- src/components/NavBar/NavDetail/NavTabs.jsx
+++ src/components/NavBar/NavDetail/NavTabs.jsx
@@ -1,33 +1,19 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-// import Tabs from '@mui/material/Tabs';
-// import Tab from '@mui/material/Tab';
-// import { Link } from '@mui/material';
+
 import { Link } from 'react-router-dom';
 import NavMenu from './NavMenu';
 import NavMenu2 from './NavMenu2';
 
-// function LinkTab(props) {
-//   return (
-//     <Tab
-//       component="a"
-//       onClick={event => {
-//         event.preventDefault();
-//       }}
-//       {...props}
-//     />
-//   );
-// }
-
 export default function NavTabs() {
-  // const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-  // const handleChange = (event, newValue) => {
-  //   setValue(newValue);
-  // };
-  // React.useEffect(() => {
-  //   localStorage.setItem('selectedTab', value.toString());
-  // }, [value]);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  React.useEffect(() => {
+    localStorage.setItem('selectedTab', value.toString());
+  }, [value]);
 
   return (
     // <>
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    <BrowserRouter>
+    <BrowserRouter basename="/Ekopolis/">
       <Suspense fallback={<CircularProgress />}>
         <App />
       </Suspense>
