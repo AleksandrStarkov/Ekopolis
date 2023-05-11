0567092bc342545bf86ddde51f7c8f33a3dfe1bc
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2023-05-11 16:05:12 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,37 @@
 // import { createTheme, ThemeProvider } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
 // import CssBaseline from '@mui/material/CssBaseline';
-import { Suspense, lazy } from 'react';
+// import { Suspense, lazy } from 'react';
+import { Suspense } from 'react';
 
-// import Hero from './Hero/Hero';
-// import About from './About/About';
+import Hero from './Hero/Hero';
+import About from './About/About';
 import NavBar from './NavBar/NavBar';
-// import Activity from './Activity/Activity';
-// import Management from './Management/Management';
-// import Structure from './Structure/Structure';
-// import Vacancies from './Vacancies/Vacancies';
-// import Agreement from './Agreement/Agreement';
-// import Population from './Population/Population';
-// import Rates from './Rates/Rates';
-// import Information from './Information/Information';
-// import News from './News/News';
-// import Contacts from './Contacts/Contacts';
+import Activity from './Activity/Activity';
+import Management from './Management/Management';
+import Structure from './Structure/Structure';
+import Vacancies from './Vacancies/Vacancies';
+import Agreement from './Agreement/Agreement';
+import Population from './Population/Population';
+import Rates from './Rates/Rates';
+import Information from './Information/Information';
+import News from './News/News';
+import Contacts from './Contacts/Contacts';
 import { CircularProgress } from '@mui/material';
 
-const Hero = lazy(() => import('./Hero/Hero'));
-const About = lazy(() => import('./About/About'));
+// const Hero = lazy(() => import('./Hero/Hero'));
+// const About = lazy(() => import('./About/About'));
 // const NavBar = lazy(() => import('./NavBar/NavBar'));
-const Activity = lazy(() => import('./Activity/Activity'));
-const Management = lazy(() => import('./Management/Management'));
-const Structure = lazy(() => import('./Structure/Structure'));
-const Vacancies = lazy(() => import('./Vacancies/Vacancies'));
-const Agreement = lazy(() => import('./Agreement/Agreement'));
-const Population = lazy(() => import('./Population/Population'));
-const Rates = lazy(() => import('./Rates/Rates'));
-const Information = lazy(() => import('./Information/Information'));
-const News = lazy(() => import('./News/News'));
-const Contacts = lazy(() => import('./Contacts/Contacts'));
+// const Activity = lazy(() => import('./Activity/Activity'));
+// const Management = lazy(() => import('./Management/Management'));
+// const Structure = lazy(() => import('./Structure/Structure'));
+// const Vacancies = lazy(() => import('./Vacancies/Vacancies'));
+// const Agreement = lazy(() => import('./Agreement/Agreement'));
+// const Population = lazy(() => import('./Population/Population'));
+// const Rates = lazy(() => import('./Rates/Rates'));
+// const Information = lazy(() => import('./Information/Information'));
+// const News = lazy(() => import('./News/News'));
+// const Contacts = lazy(() => import('./Contacts/Contacts'));
 
 // const darkTheme = createTheme({
 //   palette: {
