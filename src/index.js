fc1f90b87a84780e1c5046f3efc7d07a23c9de72
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2023-05-11 14:47:18 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    <BrowserRouter
-    // basename="/Ekopolis/"
-    >
+    <BrowserRouter basename="/Ekopolis">
       <Suspense fallback={<CircularProgress />}>
         <App />
       </Suspense>
